38fbf79b8b7ae5858198a65f209bb6f4e4c014e8
itohti <itohti@gmail.com>
2024-10-20 14:44:19 -0400

    fixed junit

---
+++ newimm-dm/src/test/SystemTesting.java
@@ -0,0 +1,9 @@
+package edu.gmu.cs321;
+
+// yall can ignore the errors.
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SystemTesting {
+    
+}
