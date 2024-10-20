a7b1734e5f7e6781986dee70fe8c1d11713dde35
itohti <itohti@gmail.com>
2024-10-20 16:01:47 -0400

    finished javadocing backend class files

--- newimm-dm/src/main/java/edu/gmu/cs321/backend/ValidateAddress.java
+++ newimm-dm/src/main/java/edu/gmu/cs321/backend/ValidateAddress.java
@@ -1,18 +1,35 @@
 package edu.gmu.cs321.backend;
-
+/**
+ * ValidateAddress will handle calling the database and validating the address variable to see if its a valid address.
+ * Class variable address will contain the address if setAddress is called otherwise the default value is null.
+ */
 public class ValidateAddress {
+    private String address = null;
 
+    /**
+     * This method will take the argument and set it to address.
+     * @param string
+     */
     public void setAddress(String string) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setAddress'");
     }
 
+    /**
+     * This method will return the address.
+     * @return String
+     */
     public String getAddress() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getAddress'");
     }
 
-    public boolean validateAddress() {
+    /**
+     * This method will go through the database and validate address.
+     * It will be important to check if the name is not null if it is throw an IllegalStateException.
+     * @return boolean
+     */
+    public boolean validateAddress() throws IllegalStateException{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'validateAddress'");
     }
--- newimm-dm/src/main/java/edu/gmu/cs321/backend/ValidateImm.java
+++ newimm-dm/src/main/java/edu/gmu/cs321/backend/ValidateImm.java
@@ -1,37 +1,74 @@
 package edu.gmu.cs321.backend;
 
+/**
+ * This class will be responsible for handling immigrant data and validating it.
+ * Class variables will be firstName, lastName, and DOB which will contain the date of birth of the immigrant.
+ * These variables will have to be set via setter and its default value will be null.
+ */
 public class ValidateImm {
 
+    private String firstName = null;
+    private String lastName = null;
+    private String DOB = null;
+
+    /**
+     * This method will set the firstName to the string passed.
+     * @param string
+     */
     public void setFirstName(String string) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setFirstName'");
     }
 
+    /**
+     * This method will get firstName and return it.
+     * @return String
+     */
     public String getFirstName() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getFirstName'");
     }
 
+    /**
+     * This method will set lastName with the String passed.
+     * @param string
+     */
     public void setLastName(String string) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setLastName'");
     }
 
+    /**
+     * This method will return lastName.
+     * @return String
+     */
     public String getLastName() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getLastName'");
     }
 
+    /**
+     * This method will set DOB to the string passed.
+     * @param string
+     */
     public void setDOB(String string) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setDOB'");
     }
 
+    /**
+     * This method will get DOB and return it.
+     * @return String
+     */
     public String getDOB() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getDOB'");
     }
 
+    /**
+     * This method will get the three class variables and validate them with the database to see if they were a previous green card holder.
+     * @return boolean
+     */
     public boolean validateImm() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'validateImm'");
