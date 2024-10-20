1d2811cde926168845b7819fb532d641404247ad
itohti <itohti@gmail.com>
2024-10-20 16:22:13 -0400

    added methods to NameComponent

--- newimm-dm/src/main/java/edu/gmu/cs321/frontend/NameComponent.java
+++ newimm-dm/src/main/java/edu/gmu/cs321/frontend/NameComponent.java
@@ -1,5 +1,25 @@
 package edu.gmu.cs321.frontend;
 
 public class NameComponent {
+
+    public void setFirstName(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setFirstName'");
+    }
+
+    public String getFirstName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFirstName'");
+    }
+
+    public void setLastName(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setLastName'");
+    }
+
+    public String getLastName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLastName'");
+    }
     
 }
