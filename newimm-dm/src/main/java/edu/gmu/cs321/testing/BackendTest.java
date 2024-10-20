00d292b699f4c5cd1940803c4fbd1643e6a26e01
itohti <itohti@gmail.com>
2024-10-20 14:23:40 -0400

    added junit testing

--- newimm-dm/src/main/java/edu/gmu/cs321/testing/BackendTest.java
+++ newimm-dm/src/main/java/edu/gmu/cs321/testing/BackendTest.java
@@ -1,5 +1,10 @@
 package edu.gmu.cs321.testing;
 
+// yall can ignore the errors.
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import edu.gmu.cs321.backend.*;
+
 /**
  * Will test the following classes
  * Validate
--- newimm-dm/src/main/java/edu/gmu/cs321/testing/FrontendTest.java
+++ newimm-dm/src/main/java/edu/gmu/cs321/testing/FrontendTest.java
@@ -1,5 +1,9 @@
 package edu.gmu.cs321.testing;
 
+// yall can ignore the errors.
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import edu.gmu.cs321.frontend.*;
 /**
  * Will test the following classes
  * AddressComponent
