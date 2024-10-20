43eebe8adbab0881e316be9a8234b7b7f11636b7
itohti <itohti@gmail.com>
2024-10-20 16:24:24 -0400

    added AddressComponent methods

--- newimm-dm/src/main/java/edu/gmu/cs321/frontend/AddressComponent.java
+++ newimm-dm/src/main/java/edu/gmu/cs321/frontend/AddressComponent.java
@@ -1,5 +1,25 @@
 package edu.gmu.cs321.frontend;
 
 public class AddressComponent {
+
+    public String getAddress() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAddress'");
+    }
+
+    public int getZipcode() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getZipcode'");
+    }
+
+    public String getCountry() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCountry'");
+    }
+
+    public String getState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getState'");
+    }
     
 }
