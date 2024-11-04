06bb44ddc4d0f01c81f9dd10ac32ecad47703507
jeshanthony <janthon8@gmu.edu>
2024-11-04 13:48:51 -0500

    AddressComponent

--- newimm-dm/src/main/java/edu/gmu/cs321/frontend/AddressComponent.java
+++ newimm-dm/src/main/java/edu/gmu/cs321/frontend/AddressComponent.java
@@ -1,51 +1,70 @@
 package edu.gmu.cs321.frontend;
+
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class AddressComponent extends VBox {
+    private TextField addressField;
+    private TextField zipcodeField;
+    private TextField countryField;
+    private TextField stateField;
+
     public AddressComponent() {
-        Label label = new Label("Address section");
-        getChildren().add(label);
+        // Create and add UI elements to the AddressComponent
+        Label addressLabel = new Label("Address:");
+        addressField = new TextField(); // TextField for address
+
+        Label zipcodeLabel = new Label("Zipcode:");
+        zipcodeField = new TextField(); // TextField for zipcode
+
+        Label countryLabel = new Label("Country:");
+        countryField = new TextField(); // TextField for country
+
+        Label stateLabel = new Label("State:");
+        stateField = new TextField(); // TextField for state
+
+        // Add all elements to the layout
+        getChildren().addAll(addressLabel, addressField, zipcodeLabel, zipcodeField, countryLabel, countryField, stateLabel, stateField);
     }
 
+    // Getter for address
     public String getAddress() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAddress'");
+        return addressField.getText();
     }
 
+    public void setAddress(String address) {
+        addressField.setText(address);
+    }
+
+    // Getter for zipcode
     public int getZipcode() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getZipcode'");
+        try {
+            return Integer.parseInt(zipcodeField.getText());
+        } catch (NumberFormatException e) {
+            return 0; // or handle as appropriate
+        }
     }
 
+    public void setZipcode(int zipcode) {
+        zipcodeField.setText(String.valueOf(zipcode));
+    }
+
+    // Getter for country
     public String getCountry() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCountry'");
+        return countryField.getText();
     }
 
+    public void setCountry(String country) {
+        countryField.setText(country);
+    }
+
+    // Getter for state
     public String getState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getState'");
+        return stateField.getText();
     }
 
-    public void setAddress(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setAddress'");
+    public void setState(String state) {
+        stateField.setText(state);
     }
-
-    public void setZipcode(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setZipcode'");
-    }
-
-    public void setCountry(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setCountry'");
-    }
-
-    public void setState(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setState'");
-    }
-    
 }
