d2bdfd8d5d5df192b7de2248764aac81c08bd61b
skytran904 <skytran904@gmail.com>
2024-11-04 14:29:52 -0500

    Added DOBComponent

--- newimm-dm/src/main/java/edu/gmu/cs321/frontend/DOBComponent.java
+++ newimm-dm/src/main/java/edu/gmu/cs321/frontend/DOBComponent.java
@@ -3,39 +3,58 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class DOBComponent extends VBox {
+
+    private TextField yearField;
+    private TextField monthField;
+    private TextField dayField;
+
     public DOBComponent() {
-        Label label = new Label("Date of birth section");
-        getChildren().add(label);
+        Label yearLabel = new Label("Year: ");
+        yearField = new TextField();
+
+        Label monthLabel = new Label("Month: ");
+        monthField = new TextField();
+
+        Label dayLabel = new Label("Day: ");
+        dayField = new TextField();
+
+        getChildren().addAll(yearLabel, yearField, monthLabel, monthField, dayLabel, dayField);
     }
 
-    public void setYear(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setYear'");
+    public void setYear(int year) {
+        yearField.setText(String.valueOf(year));
     }
 
-    public void setMonth(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setMonth'");
+    public void setMonth(int month) {
+        monthField.setText(String.valueOf(month));
     }
 
-    public void setDay(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setDay'");
+    public void setDay(int day) {
+        dayField.setText(String.valueOf(day));
     }
 
     public int getYear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getYear'");
+        try {
+            return Integer.parseInt(yearField.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 
     public int getMonth() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMonth'");
+        try {
+            return Integer.parseInt(monthField.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 
     public int getDay() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDay'");
+        try {
+            return Integer.parseInt(dayField.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
     
 }
